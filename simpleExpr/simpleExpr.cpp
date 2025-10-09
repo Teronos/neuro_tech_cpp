9a434d53511671a52ef18496e09ae45b6cfdf274
Vladislav Kazankov <teronos@qfteam.org>
2025-10-09 17:56:17 +0300

    Squashed commit of the following:
    commit a824d09ac43ec76ab2a52107df9f7c8a5123ac3b
    Author: Vladislav Kazankov <teronos@qfteam.org>
    Date:   Thu Oct 9 17:42:53 2025 +0300
    
        create gitignore; add first problem
    
    commit b632f9e47c07a08604c24cb9541dbeb14f445dc2
    Author: Vladislav Kazankov <teronos@qfteam.org>
    Date:   Thu Sep 11 19:57:19 2025 +0300
    
        first consol app
    
    commit 6c2b85dc93d993d82d659efadcaf1f787bb8aa7d
    Author: Vladislav Kazankov <teronos@qfteam.org>
    Date:   Thu Sep 11 19:03:27 2025 +0300
    
        feat: add fake c++ file

---
+++ simpleExpr/simpleExpr.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+namespace
+{
+    void argregatorPersolalInfo(int age, double weight) {
+
+        double height;
+        wstring name{ L"Vladislav" }, description;
+
+        wcout << "Name: " << name << "\n";
+        wcout << "Age: " << age << " Weight: "
+            << weight << endl;
+
+        {
+            wcout << "Enter height:" << endl;
+            cin >> height;
+            wcin >> description;
+        }
+    }
+
+    void argregatorPersolalInfo()
+    {
+        argregatorPersolalInfo(27, 77.3);
+    }
+}
+
+namespace problems
+{
+    // TO DO:
+    /*
+    * Дано трехзначное натуральное число N.
+    * Нужно найти сумму цифр числа N
+    */
+    int problem1() {
+        int N;
+        std::cin >> N;
+        return N % 10 + (N / 10) % 10 + N / 100;
+    }
+
+    int problem1AlexR() {
+        int N;
+        std::cin >> N;
+
+        auto acc{ 0 };
+        while (N) {
+            //acc = acc + N % 10;
+            acc += N % 10;
+            N /= 10;
+        }
+
+        return acc;
+    }
+
+    int problem1Diamond() {
+        std::string number_str;
+        std::cin >> number_str;
+
+        auto it = number_str.begin();
+
+        auto acc{ 0 };
+        //for (int index = 0; index < number_str.length(); index++)
+        //    acc += number_str[index] - '0';
+
+        for (auto symbol : number_str)
+            acc += symbol - '0';
+        return acc;
+    }
+}
+
+
+int main() {
+    //argregatorPersolalInfo();
+    auto result = problems::problem1Diamond();
+    system("pause");
+}
